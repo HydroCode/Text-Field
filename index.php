25c37e17e86991e0ab39a34bfaac75aed81d5fe7
Yashas Shah <79524299+coolcoder-js@users.noreply.github.com>
2021-05-23 21:13:35 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>HydroCode - Text Field</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <script src="script.js"></script>
+		<div id="app-6">
+  		<p>{{ message }}</p>
+  		<input v-model="message">
+		</div>
+  </body>
+</html>
